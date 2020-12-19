31c68c64acc9502984e7ec2bbd0ccaee2b5b5f51
Clement hard <clement.hard.contact@gmail.com>
2020-12-19 14:49:00 +0100

    maj

---
+++ Client NTP/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client_NTP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient client;
+            byte[] trame = new byte[48];
+            byte[] recu;
+            IPEndPoint iep = new IPEndPoint(0, 0);
+            // Construction de la trame à envoyer au serveur NTP
+            // Champ VN à 4, champ Mode à 3, donc le premier octet de cette trame vaut :
+            // 0 1 2 3 4 5 6 7
+            // +-+-+-+-+-+-+-+-+
+            // |LI | VN |Mode |
+            // +-+-+-+-+-+-+-+-+
+            // +0 0|1 0 0|0 1 1+
+            // soit 2 | 3 en hexadécimal
+            trame[0] = 0x23;
+            try
+            {
+                client = new UdpClient("ntp.u-picardie.fr", 123);
+                // Envoi de la trame au serveur NTP
+                client.Send(trame, 48);
+                // Réception de la trame de réponse – iep contient l’identité de l’émetteur du message
+                recu = client.Receive(ref iep);
+                Console.WriteLine("Réponse reçue du serveur {0}", iep.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+            // Destruction de la socket;
+            client.Close();
+            // Passage de big endian vers litte endian du champ TT de la trame reçue
+            Array.Reverse(recu, 40, 4);
+            // Extraction de ce champ TT dans un entier non signé
+            uint TT = BitConverter.ToUInt32(recu, 40);
+            // Ajout du nb de seconde contenu dans TT à 01/01/1900
+            DateTime heure = new DateTime(1900, 1, 1);
+            heure = heure.AddSeconds(TT).ToLocalTime();
+            // Affichage du résultat obtenu
+            Console.WriteLine("Heure exacte : {0}", heure.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Net.Sockets;
+
+
+namespace Server
+{
+    class Program
+    {
+        // Programme principal
+        static void Main(string[] args)
+        {
+            IPEndPoint iep = new IPEndPoint(IPAddress.Any, 1212);
+            try
+            {
+                // Etape 1
+                Socket sock_ecoute = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                // Etape 2
+                sock_ecoute.Bind(iep);
+                //Etape 3
+                sock_ecoute.Listen(5);
+                while (true)
+                {
+                    // Etape 4.1
+                    Socket sock_serv = sock_ecoute.Accept();
+                    // Etape 4.2
+                    ThreadPool.QueueUserWorkItem(Communique, sock_serv);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return;
+            }
+        }
+
+        // Sous-programme de communication avec le client
+        // Paramètre d'entrée. Arg : socket utilisée pour la communication
+        // Valeur de retour. Aucune
+        static void Communique(object arg)
+        {
+            Socket sock = (Socket)arg;
+            byte[] b_an = new byte[4];
+            byte[] b_si = new byte[8];
+
+            Console.WriteLine("{0} - Client : {1}", DateTime.Now, sock.RemoteEndPoint.ToString());
+
+            // Récupération du nb d'année et du capital initial envoyés par le client
+            sock.Receive(b_an);
+            sock.Receive(b_si);
+
+            // Conversion tableau d'octets -> nombres
+            int nbAnnees = BitConverter.ToInt32(b_an, 0);
+            double si = BitConverter.ToDouble(b_si, 0);
+
+            // Calcul du capital final
+            for (int i = 0; i < nbAnnees; i++)
+                si = si + (si * 0.75 / 100);
+
+            // Conversion nombre -> tableau d'octets
+            byte[] b_sf = BitConverter.GetBytes(si);
+
+            // Envoi au client
+            sock.Send(b_sf);
+
+            // Désactivation et destruction de la socket
+            sock.Shutdown(SocketShutdown.Both);
+            sock.Close();
+        }
+    }
+}
